afbb01a8c6e52b4149722f4d8782c87ec0274774
ketanDeshmukh <deshmukhketan647@gmail.com>
2023-01-11 04:08:13 +0530

    "This is done"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   template: `
+				<app-user></app-user>
+             `
 })
-export class AppComponent {
-  title = 'Angular-FormGroup-setValue-and-patchValue';
+export class AppComponent { 
 }
+    
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+      imports: [
+            BrowserModule,
+            ReactiveFormsModule
+      ],
+      declarations: [
+            AppComponent,
+            UserComponent
+      ],
+      providers: [
+      ],
+      bootstrap: [
+            AppComponent
+      ]
 })
 export class AppModule { }
